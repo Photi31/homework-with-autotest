5914d2765369bf35c51899f5789c2f6e5d197c69
svetlana nemova <photiniya@gmail.com>
2023-02-05 21:55:06 +0300

    5, 7 is done

--- src/s2-homeworks/hw02/affairs/Affairs.tsx
+++ src/s2-homeworks/hw02/affairs/Affairs.tsx
@@ -4,9 +4,9 @@ import {AffairType, FilterType} from '../HW2'
 import s from './Affairs.module.css'
 
 type AffairsPropsType = {
-    data: any // need to fix any
+    data: AffairType[] // need to fix any
     setFilter: any
-    deleteAffairCallback: any
+    deleteAffairCallback: (_id: number) => void
     filter: FilterType
 }
 
