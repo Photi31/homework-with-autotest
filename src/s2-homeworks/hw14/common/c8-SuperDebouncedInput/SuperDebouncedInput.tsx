b3d9fbd1fbba664962edb163873e0d4402e08fda
Photi31 <photiniya@gmail.com>
2023-05-02 15:44:39 +0300

    HW 14, may be fixed bug.

--- src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
+++ src/s2-homeworks/hw14/common/c8-SuperDebouncedInput/SuperDebouncedInput.tsx
@@ -33,10 +33,10 @@ const SuperDebouncedInput: React.FC<SuperDebouncedInputPropsType> = (
 
         if (onDebouncedChange) {
             // делает студент
-            clearInterval()
-            setTimerId(+setTimeout(() => {
-                onDebouncedChange(value)
-            }, 1500))
+
+
+            clearTimeout(timerId)
+            setTimerId(+setTimeout(() => onDebouncedChange(value), 1500))
 
             // остановить предыдущий таймер
             // запустить новый на 1500ms, в котором вызовется функция
